3ec10958809fa0f571f0408c9f4d6f9fe54ba650
Ivã Munhoz <iamunhoz@gmail.com>
2022-10-07 19:54:58 -0300

    creation of cpt TopBar

--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -1,3 +1,30 @@
+import { useAppStore } from 'state'
+
 export default function TopBar() {
-  return <div>TopBar</div>
+  const { setPostId } = useAppStore((state) => state)
+
+  const goToTimeline = () => {
+    setPostId('0')
+  }
+  const openContactFormModal = () => {
+    //
+  }
+  return (
+    <div
+      className="flex h-24 items-center justify-around"
+      style={{
+        background: '#2D2D2D'
+      }}
+    >
+      <h1 className="text-4xl font-bold">Rockr Blog</h1>
+      <div>
+        <button onClick={goToTimeline} className="mr-12 text-2xl text-white">
+          Posts
+        </button>
+        <button onClick={openContactFormModal} className="text-2xl text-white">
+          Contact
+        </button>
+      </div>
+    </div>
+  )
 }
